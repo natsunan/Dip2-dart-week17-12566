a81f1dd706f94b53d8842aaee1c474ec510dd81a
Natsunsn <Natsunan270@gmail.com>
2023-09-07 15:55:14 +0700

    first commit

---
+++ bin/dart_week17.dart
@@ -0,0 +1,17 @@
+import 'package:dart_week17/no_param_no_return.dart';
+import 'package:dart_week17/no_param_return.dart';
+import 'package:dart_week17/param_no_return.dart';
+import 'package:dart_week17/param_return.dart';
+import 'package:dart_week17/positional_param_.dart';
+
+void main(){
+ protoType();
+}
+void protoType (){
+  setInfo();
+  setParam();
+  setMessage();
+  setNameNopNoreturn();
+  setName();
+}
+
---
+++ lib/no_param_no_return.dart
@@ -0,0 +1,8 @@
+//1.No Parameter Ant No Return Type
+void setNameNopNoreturn(){
+  printNopNoreturn();
+}
+
+ void printNopNoreturn(){
+  print("Mr.Jack Ma");
+}
